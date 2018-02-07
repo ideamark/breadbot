964a329c8a64bfb7fc7dcb955f2c42bcf169194a
Mark Young <ideamark@qq.com>
2018-02-07 19:10:38 +0800

    modified:   check_data.py

--- breadbot/data/check_data.py
+++ breadbot/data/check_data.py
@@ -11,33 +11,34 @@ class checkData(object):
     def __init__(self):
         pass
 
-    def _error(self, path, err, msg):
-        print('\n%s\n[Error] %s' % (path, msg))
+    def _error(self, err, msg):
+        print('\n[Error] %s' % msg)
         raise Exception(err)
 
     def do_check(self, dataPaths=[]):
         try:
             dataPaths = common.path_parser(dataPaths)
             for dataPath in dataPaths:
+                print('Checking %s...' % dataPath)
                 if not os.path.exists(dataPath):
-                    self._error(dataPath, '', 'wrong data path')
+                    self._error('', 'wrong data path')
 
                 with open(dataPath, 'r') as f:
                     qas = yaml.load(f.read())['qas']
 
                 for qa in qas:
                     if not qa:
-                        self._error(dataPath, qa, 'qa is none')
+                        self._error(qa, 'qa is none')
                     for value in qa.values():
                         if not value:
-                            self._error(dataPath, qa, 'value is none')
+                            self._error(qa, 'value is none')
                         else:
                             for item in value:
                                 if type(item) == dict:
-                                    self._error(dataPath, qa, 'item is dict')
+                                    self._error(qa, 'item is dict')
                                 if not item:
-                                    self._error(dataPath, qa, 'item is none')
-            print('\nCheck Passed!')
+                                    self._error(qa, 'item is none')
+                print('\nCheck Passed!')
 
         except Exception as e:
             print(e)
