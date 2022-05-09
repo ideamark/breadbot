c2eef71fee32f283c579436892c44c6761a6fc39
Mark Yang <mark@Marks-MacBook.local>
2022-05-10 00:08:50 +0800

    remove sudo from setup.py

--- setup.py
+++ setup.py
@@ -3,73 +3,77 @@ import os
 import sys
 import platform
 from setuptools import setup
-
-
-def check_sudo():
-    if os.geteuid():
-        args = [sys.executable] + sys.argv
-        os.execlp('sudo', 'sudo', *args)
+import traceback
 
 
 def check_system():
     return platform.system()
 
 
-def install():
-    os.system('pip3 install -U pip')
-    os.system('pip3 install -r requirements.txt')
-    setup(
-        setup_requires=['pbr>=0.1'],
-        pbr=True,)
+def get_home_dir():
+    os_platform = check_system()
+    if os_platform == 'Windows':
+        return os.environ['USERPROFILE']
+    else:
+        return os.environ['HOME']
 
-    from breadbot.core.common import Cfg
-    log_path = Cfg().get('local', 'log_path')
-    mem_path = Cfg().get('local', 'mem_path')
-    if not os.path.exists(log_path):
-        os.mkdir(log_path)
-    if not os.path.exists(mem_path):
-        os.mkdir(mem_path)
-    data_path = os.path.join(os.getcwd(), 'data')
-    if not os.path.exists(data_path):
-        Cfg().write('local', 'data_paths', data_path)
-        os.system(
-            'git clone https://github.com/ideamark/ideamark.github.io data')
-        os.system('chmod -R 777 data')
-    print('Install success!')
+
+def install():
+    try:
+        os.system('pip3 install -U pip')
+        os.system('pip3 install --user -r requirements.txt')
+        setup(
+            setup_requires=['pbr>=0.1'],
+            pbr=True,)
+
+        from breadbot.core.common import Cfg
+        home_dir = get_home_dir()
+        log_path = Cfg().get('local', 'log_path').replace('~', home_dir)
+        mem_path = Cfg().get('local', 'mem_path').replace('~', home_dir)
+        data_path = Cfg().get('local', 'data_path').replace('~', home_dir)
+        if not os.path.exists(log_path):
+            os.makedirs(log_path)
+        if not os.path.exists(mem_path):
+            os.makedirs(mem_path)
+        os.system('git clone https://github.com/ideamark/ideamark.github.io ' + data_path)
+        print('Install Success')
+    except Exception:
+        print(traceback.format_exc())
+        print('Install Failed')
 
 
 def uninstall():
     try:
-        from breadbot.core.common import Cfg
-        cfg_path = Cfg().get('local', 'cfg_path')
-        bin_path = Cfg().get('local', 'bin_path')
-        if os.path.exists(cfg_path):
-            os.remove(cfg_path)
-        if os.path.exists(bin_path):
-            os.remove(bin_path)
+        home_dir = get_home_dir()
         os.system('pip3 uninstall breadbot')
+        os.remove(os.path.join(home_dir, '.breadbot'))
+        print('Uninstall Success')
     except Exception:
-        pass
-    print('Uninstall success!')
+        print(traceback.format_exc())
+        print('Uninstall Failed.')
 
 
 def clean():
-    exclude = [
-        '.eggs',
-        'AUTHORS',
-        'breadbot.egg-info',
-        'build',
-        'ChangeLog'
-    ]
-    file_list = os.listdir('.')
-    for f in file_list:
-        if f in exclude:
-            os.system('rm -rf %s' % f)
-    os.system('find -name "__pycache__"|xargs rm -rf')
-    print('Clean up success!')
+    try:
+        exclude = [
+            '.eggs',
+            'AUTHORS',
+            'breadbot.egg-info',
+            'build',
+            'ChangeLog'
+        ]
+        file_list = os.listdir('.')
+        for f in file_list:
+            if f in exclude:
+                os.system('rm -rf %s' % f)
+        os.system('find -name "__pycache__"|xargs rm -rf')
+        print('Clean up Success')
+    except Exception:
+        print(traceback.format_exc())
+        print('Clean up Failed')
 
 
-def linux_menu():
+def menu():
     if len(sys.argv) <= 1:
         print('Please enter install, uninstall or clean')
         sys.exit(1)
@@ -88,7 +92,6 @@ def linux_menu():
 if __name__ == '__main__':
     os_platform = check_system()
     if os_platform == 'Windows':
-        print('Windows support is developing, will come soon.')
-    elif os_platform == 'Linux':
-        check_sudo()
-        linux_menu()
+        print('Windows is not supported yet.')
+    else:
+        menu()
