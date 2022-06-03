b3c5fe541b4d37cd52527ec8dc875f8870129621
Mark Yang <mark@Marks-MacBook.local>
2022-06-03 23:14:54 +0800

    update

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ pip3 install --user -r requirements.txt
 python3 setup.py install
 cp -r lib/ ~/.breadbot/
 git clone https://github.com/ideamark/ideamark.github.io ~/.breadbot/data
+sed -in-place -e 's%~%'${HOME}'%g' ~/.breadbot/etc/bread.cfg
